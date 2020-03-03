a1ba008410a1b7fad591ae3faf0eaedef2b5736c
Josh Mu <mu@joshmu.com>
2020-03-03 17:17:53 +1000

    added Blog title plus removed comments from tut

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,13 +3,6 @@ import { graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 
-// SHOW LIST OF POSTS ON BLOG PAGE
-// query title and date for each post in blog component
-// render an ol on the page
-// render a lit with a nested h2 (title) and p (date) for each post
-// -- "render array of object"
-// test
-
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -28,9 +21,10 @@ const BlogPage = () => {
 
   return (
     <Layout>
+      <h1>Blog</h1>
       <ol>
-        {data.allMarkdownRemark.edges.map((edge, idx) => (
-          <li key={idx}>
+        {data.allMarkdownRemark.edges.map(edge => (
+          <li key={edge.id}>
             <h2>{edge.node.frontmatter.title}</h2>
             <p>{edge.node.frontmatter.date}</p>
           </li>
