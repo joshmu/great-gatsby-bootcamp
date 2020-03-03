f7393c1510a49d146c2e415a939b48d208c9df63
Josh Mu <mu@joshmu.com>
2020-03-03 16:25:55 +1000

    graphql author for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,22 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
+
   return (
     <footer>
-      <p>Created by Josh Mu, ©{` ${new Date().getFullYear()}`}</p>
+      <p>
+        Created by {data.site.siteMetadata.author}, © {new Date().getFullYear()}
+      </p>
     </footer>
   )
 }
