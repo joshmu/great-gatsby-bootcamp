dc3fe94beb74fc12547f12d06638498d112c76e3
Josh Mu <mu@joshmu.com>
2020-03-03 16:56:27 +1000

    setup gatsby file source and remark plugin to use markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,15 @@ module.exports = {
     title: "Full-Stack Bootcamp.",
     author: "Josh Mu",
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,13 @@ import React from "react"
 
 import Layout from "../components/layout"
 
+// SHOW LIST OF POSTS ON BLOG PAGE
+// query title and date for each post in blog component
+// render an ol on the page
+// render a lit with a nested h2 (title) and p (date) for each post
+// -- "render array of object"
+// test
+
 const BlogPage = () => {
   return (
     <Layout>
