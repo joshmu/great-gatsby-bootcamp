af3099941c57b036843743be18ca4b1c68f23947
Josh Mu <mu@joshmu.com>
2020-03-04 16:15:44 +1000

    contentful cms setup

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,8 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 
+import blogStyles from "./blog.module.scss"
+
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -25,9 +27,9 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map((edge, idx) => (
-          <li key={idx}>
+          <li key={idx} className={blogStyles.post}>
             <Link to={`/blog/${edge.node.fields.slug}`}>
               <h2>{edge.node.frontmatter.title}</h2>
               <p>{edge.node.frontmatter.date}</p>
