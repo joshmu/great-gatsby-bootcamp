0092258086dc3c1c96ca99bd12f6c04c475c16fa
Josh Mu <mu@joshmu.com>
2020-03-04 16:58:47 +1000

    commented file gatsby-source-contentful > src > fragments.js due to error

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,16 +8,12 @@ import blogStyles from "./blog.module.scss"
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
@@ -28,11 +24,11 @@ const BlogPage = () => {
     <Layout>
       <h1>Blog</h1>
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map((edge, idx) => (
+        {data.allContentfulBlogPost.edges.map((edge, idx) => (
           <li key={idx} className={blogStyles.post}>
-            <Link to={`/blog/${edge.node.fields.slug}`}>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
+            <Link to={`/blog/${edge.node.slug}`}>
+              <h2>{edge.node.title}</h2>
+              <p>{edge.node.date}</p>
             </Link>
           </li>
         ))}
